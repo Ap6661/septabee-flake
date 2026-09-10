31b26ab4a7261a2346e129a3aa3e04bab2f778bf
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2026-09-10 13:38:06 -0400

    Extract Verison list to its own file.

---
+++ versions.nix
@@ -0,0 +1,19 @@
+{
+  latest = "B_T7";
+  latest_offline = "B_T7_offline";
+
+  hashes = {
+    "B_T7" = "sha256-+BRkf4TrJZqCjnQmUg0sMoJTsmnSyXiXNovTKo72k54=";
+    "B_T7_offline" = "sha256-tVxMALnZewmrKTWnn5C0mNmvfM84scScbwGl0Hfk+LA=";
+
+    "B_T6" = "sha256-tkBRI8GcpOjtZs9sA0ycIPeq6eaFjDVH2YBfTXl7Leo=";
+    "B_T6_offline" = "sha256-f2oR3nxSrBZHPhPN0eWWZlbFSQa34EWBEDyQmoRjvAA=";
+    "B_T5" = "sha256-dBEdBy8PChrAxiTLYoIdOqTg77UsTGoogKhV1o6eiAk=";
+
+    "B_T5_offline" = "sha256-+c8RUdz1EIPhubxDFYfhnevQN0v4cRKDQ2bcRmLn4sk=";
+    "B_T4" = "sha256-Uuu3g11TCczOSDx15AqEJTosPkPjBNaWjBAPFf8uNw8=";
+    "B_T3" = "sha256-vdXJ4Qusvi/ehztmp2iibiFZLJvbU7+mRnR7KSmxrFA=";
+    "B_T2" = "sha256-OMnbRBTku8yi4b3Ay7d70EbB/e2Qh+PfzK2O8qRFoaA=";
+    "B_T1" = "sha256-JlWmeDnMTjBNwLTADvSswbtfhJK6t1bu0xHkmBgLtvA=";
+  };
+}
