7d75c5a0d466c8e2195ff85f54d40813603af990
APnda <apnda6661@proton.me>
2026-09-23 15:45:32 -0500

    Update T13

--- versions.nix
+++ versions.nix
@@ -1,8 +1,10 @@
 {
-  latest = "B_T12";
-  latest_offline = "B_T12_offline";
+  latest = "B_T13";
+  latest_offline = "B_T13_offline";
 
   hashes = {
+    "B_T13" = "sha256-qUInrE+kyS7DA/wCRFvKLZSIDvCdfvkstOlh+r9H3ZY=";
+    "B_T13_offline" = "sha256-hEHR7TdB7u2+akx32ekCA3xpx964fbeixsSxLA0bgko=";
     "B_T12" = "sha256-MhvCjxhFaU7VlsEGLK7oRcQfmrk2Eqv3w08J2sqs2Fc=";
     "B_T12_offline" = "sha256-uIlVS0GLIL2z36Ino9qVompIgX6g0d3L45MmdmIusYY=";
     "B_T11" = "sha256-2U3AkX5/+ZMxcCND1lkJr0WOuQarmsuThMPiNeGjPQ8=";
